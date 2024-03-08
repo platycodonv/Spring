8b87348fea2a6b3ba2dd5a623364fccec116fbba
성게 <platycodonv@github.com>
2024-03-09 00:29:11 +0900

    feat: #4 Srping으로 변환
    - @Configuration 을 통해 AppConfig 빈 팩토리로 변경
    - 스프링 빈 조회 테스트

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,15 +4,20 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
+        // AppConfig appConfig = new AppConfig();
+        // MemberService memberService = appConfig.memberService();
 
-        //MemberService memberService = new MemberServiceImpl();
-        MemberService memberService = appConfig.memberService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        //bean 이름은 기본적으로 메소드 이름으로 등록된다
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member memberA = new Member(1L, "memberA", Grade.VIP);
         memberService.join(memberA);
 
